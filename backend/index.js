3fc8c8217e06c7495e118982a487a7e70615da06
Usman Ali <prousman1@outlook.com>
2025-12-26 00:38:28 +0500

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+  origin: "https://blood-donation-bank-r68g.vercel.app/",
+  credentials: true,
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/donors", donorRoutes);
